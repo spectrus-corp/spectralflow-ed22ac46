9495b80cc5a961dae87dc7d0054917ade67f2f86
Spectrus <spectrus33160@gmail.com>
2026-05-31 16:28:57 +0000

    Clear repository

--- supabase/migrations/20260514104445_3a47d49b-a7ce-4b60-a9d6-e1b9c9628682.sql
+++
@@ -1,2 +0,0 @@
-
-revoke execute on function public.handle_new_user() from public, anon, authenticated;
--- supabase/migrations/20260518180340_09e17fb1-544c-4bcd-8952-1223bf8a93ae.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE public.subscriptions
-  ADD CONSTRAINT subscriptions_target_id_fkey
-  FOREIGN KEY (target_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
-
-ALTER TABLE public.subscriptions
-  ADD CONSTRAINT subscriptions_subscriber_id_fkey
-  FOREIGN KEY (subscriber_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
